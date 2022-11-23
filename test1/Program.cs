0b427867b3e5bbde3fb4a133a43979bfec0050d2
marina vorushina <butorinamarina@gmail.com>
2022-11-23 22:16:36 +0500

    problem with test1

--- test1/Program.cs
+++ test1/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Void ZadachaReplacetext()
+
+{   
+        string text = "Дуб — дерево."
+        + "Оптика — раздел физики."
+        + "Старший брат — мой учитель."
+        + "Старший брат мой - учитель.";
+
+        string Replace (string text, char oldValue, char newValue)
+    {
+        string result = String.Empty;
+
+        int lenght = text.lenght;
+        for (int i = 0; i < lenght; i++)
+        {
+            if(text [i] ==oldValue) resalt = result + $"{newValue}";
+            else result = result + $"{text [i]}";
+        }
+        
+        return result;
+    }
+
+    string newText = Replace(text, ' ', '|');
+    Console.WriteLine(newText);
+}
+Void ZadachaReplacetext();
